81f892920d5e7c022caad1a39ed7c2e32b69b5c8
Quintin Henn <quintin.henn@gmail.com>
2025-04-16 11:12:57 +0200

    Packtpub-Unity-Tutorials order and grouped

---
+++ Packtpub-Unity-Tutorials/source/Packtpub-Learn-Unity-3D/Assets/learnunity3dprogrammingwithunityscript/Scripts/Forces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forces : MonoBehaviour
+{
+	public float aForce = 5;
+	
+	private Rigidbody rb;
+	
+	public void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	public void FixedUpdate () {
+		//rb.AddForce (Vector3.right * aForce);
+		//rb.AddTorque (Vector3.right * aForce );
+	}
+	
+	public void OnMouseDown () 
+	{
+		rb.AddForce (Vector3.forward * aForce);
+		//rb.AddTorque (Vector3.right * aForce);
+	}
+}
---
+++ Packtpub-Unity-Tutorials/source/Packtpub-Learn-Unity-3D/Assets/learnunity3dprogrammingwithunityscript/Scripts/PointCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCamera : MonoBehaviour
+{
+	public Transform cameraTarget;
+	
+	void Update () {
+		transform.LookAt(cameraTarget);
+	}
+}
